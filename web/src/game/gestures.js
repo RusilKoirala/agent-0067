18cc6b41672c30ab5a6f0b6b9e939bcf3ca2ca2f
Rusil Koirala <rusilkoirala23@gmail.com>
2026-08-01 02:46:12 +0545

    Basic game config and gestures added

---
+++ web/src/game/gestures.js
@@ -0,0 +1,19 @@
+// all the  gestrues for the game 
+
+// :D the wonderful 6-7 poses
+
+export function parsePoseControls(landmarks, refs)  {
+    const { headCenterXRef, headCenterZRef, playerXRef, playerDepthRef, currentStateRef} = refs;
+
+
+    // all the important parts to know before just understanding 6-7 movement   
+    const nose = landmarks[0];
+    const leftShoulder = landmarks[11];
+    const rightShoulder = landmarks[12];
+    const leftWrist = landmarks[15];
+    const rightWrist = landmarks[16];
+    const leftHip = landmarks[23];
+    const rightHip = landmarks[24];
+
+
+}
---
+++ web/src/hooks/gameConfig.js
@@ -0,0 +1,10 @@
+
+// all the game constants like different constants
+/*
+guys pls dont change the following values
+*/
+export const GAME_COFING = {
+    CANVAS_WIDTH:  800,
+    CANVAS_HEIGHT: 600,
+    GAME_DURATION_SEC : 60,
+}
